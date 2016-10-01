dd5a507bc24b762c92c65ecda9ff0b130423454f
Limezest <cyril@sabourau.lt>
2016-10-01 17:59:44 +0200

    database sql

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # matchpoint-api
+Project description
+
+The main idea is to create a platform where every sport club can register itself, providing services to their members : calendars, leaderboards, chatrooms, photo sharing, etc.
+
+
+Clubs :
+* Teams
+* Matches : incoming + previous with scores
+* Championship (leaderboard)
+* Players 
+* Pictures
+
+
+Functionnalities :
+* Follow a team : receive notifications (incoming matches, new pictures, scores of the other teams in the championship)
+* Identify yourself as a player of the team (needs validation by an admin of the club)
+* Identify yourself as a coach of the team (needs validation by an admin of the club)
+* Access the Private chat for the team (sends notifications to the team members)
+* Edit teams : (club admin only)
+* Add, edit, delete a team
+* Add, edit, delete a team member
+
+
+News feed 
+* Articles
+(Ads)
+* Functionnalities :
+* Post an article (admin only) : Title, text and media (picture, video, etc.) 
+* Comment on an article (any user)
+
+
+Practical information
+* Gymnasium(s) : address and localisation
+* Contacts :
+* Main mail address
+* Main email address
+* Main phone number
+* List of people to contact
+* Functionnalities :
+* Send email
+* Send a private message
+* Call phone number
+* Edit list of people to contact (club admin only)
+* Edit gymnasiums (club admin only)
+* Edit main mail, email, phone numbers (club admin only)
+
+
+eShop (no payment online, just add the ability to create an order and send it to an admin)
+* Product list
+* My cart
+
+
+Administration (club admin only)
+* Functionnalities :
+* Edit practical information
+* Edit logo
+* Edit color theme
+
+
+Chatrooms
+* Functionnalities :
+* Access to the chatrooms of the club you are related to
+* Access to the chatrooms of the teams you are related to
+* Access to the chatrooms you created by contacting other users
---
+++ db.sql
@@ -0,0 +1,129 @@
+﻿DROP TABLE IF EXISTS UTILISATEUR;
+Create Table UTILISATEUR
+(
+    utilisateurID   INTEGER NOT NULL UNIQUE,
+    firstname       CHAR(15) NOT NULL,
+    lastname        CHAR(15) NOT NULL,
+    mail            CHAR(50),
+    email           CHAR(50) NOT NULL UNIQUE,
+    phone           CHAR(20) UNIQUE,
+    birthday        CHAR(10),
+    isAdmin         BOOLEAN DEFAULT FALSE,
+    isPlayer        BOOLEAN DEFAULT FALSE,
+    isCoach         BOOLEAN DEFAULT FALSE,
+    CONSTRAINT utilisateurID PRIMARY KEY(utilisateurID)
+);
+
+
+DROP TABLE IF EXISTS CLUB;
+CREATE TABLE CLUB
+(
+    clubID      INTEGER NOT NULL UNIQUE,
+    adminID     INTEGER REFERENCES UTILISATEUR(utilisateurID) NOT NULL,
+    name        CHAR(20) NOT NULL,
+    picture     CHAR(30),
+    logo        CHAR(30),
+    color       CHAR(15),
+    mail        CHAR(50),
+    email       CHAR(50) NOT NULL UNIQUE,
+    phone       CHAR(20)UNIQUE,
+    CONSTRAINT clubID PRIMARY KEY(clubID)
+);
+
+
+DROP TABLE IF EXISTS TEAM;
+CREATE TABLE TEAM
+(
+    teamID  INTEGER NOT NULL UNIQUE,
+    clubID  INTEGER REFERENCES CLUB(clubID) NOT NULL,
+    coachID INTEGER REFERENCES UTILISATEUR(utilisateurID),
+    name    CHAR(20),
+    CONSTRAINT teamID PRIMARY KEY(teamID)
+);
+
+
+DROP TABLE IF EXISTS UTILISATEUR_TEAM;
+CREATE TABLE UTILISATEUR_TEAM
+(
+    utilisateurID   INTEGER REFERENCES UTILISATEUR(utilisateurID) NOT NULL,
+    teamID          INTEGER REFERENCES TEAM(teamID) NOT NULL
+);
+
+
+DROP TABLE IF EXISTS FOLLOW_TEAM;
+CREATE TABLE FOLLOW_TEAM
+(
+    utilisateurID   INTEGER REFERENCES UTILISATEUR(utilisateurID) NOT NULL,
+    teamID          INTEGER REFERENCES TEAM(teamID) NOT NULL
+);
+
+
+DROP TABLE IF EXISTS GYMNASIUM;
+CREATE TABLE GYMNASIUM
+(
+    gymnasiumID INTEGER NOT NULL UNIQUE,
+    name        CHAR(30) NOT NULL,
+    location    CHAR(50) NOT NULL,
+    email       CHAR(50),
+    phone       CHAR(20),
+    capacity    INTEGER,
+    CONSTRAINT gymnasiumID PRIMARY KEY(gymnasiumID)
+);
+
+
+DROP TABLE IF EXISTS CS_LEVEL;
+CREATE TABLE CS_LEVEL
+(
+    cs_levelID  INTEGER NOT NULL UNIQUE,
+    value       CHAR(20),
+    CONSTRAINT cs_levelID PRIMARY KEY(cs_levelID)
+);
+
+
+DROP TABLE IF EXISTS CS_AGE;
+CREATE TABLE CS_AGE
+(
+    cs_ageID    INTEGER NOT NULL UNIQUE,
+    value       CHAR(20),
+    CONSTRAINT cs_ageID PRIMARY KEY(cs_ageID)
+);
+
+
+DROP TABLE IF EXISTS CHAMPIONSHIP;
+CREATE TABLE CHAMPIONSHIP
+(
+    championshipID      INTEGER NOT NULL UNIQUE,
+    name                CHAR(30) NOT NULL UNIQUE,
+    level               INTEGER REFERENCES CS_LEVEL(cs_levelID),
+    age                 INTEGER REFERENCES CS_AGE(cs_ageID),
+    masculinFeminin     CHAR(1),
+    CONSTRAINT championshipID PRIMARY KEY(championshipID)
+);
+
+DROP TABLE IF EXISTS MATCH;
+CREATE TABLE MATCH
+(
+    matchID                 INTEGER NOT NULL UNIQUE,
+    championshipID          INTEGER REFERENCES CHAMPIONSHIP(championshipID),
+    gymnasiumID             INTEGER REFERENCES GYMNASIUM(gymnasiumID),
+    date_d                  DATE NOT NULL,
+    team1ID                 INTEGER REFERENCES TEAM(teamID),
+    team2ID                 INTEGER REFERENCES TEAM(teamID),
+    scoreTeam1              INTEGER,
+    scoreTeam2              INTEGER,
+    teamQuiAccueilleID      INTEGER REFERENCES TEAM(teamID),
+    CONSTRAINT matchID PRIMARY KEY(matchID)
+);
+
+
+DROP TABLE IF EXISTS TEAM_CHAMPIONSHIP;
+CREATE TABLE TEAM_CHAMPIONSHIP
+(
+    teamID          INTEGER REFERENCES TEAM(teamID) NOT NULL,
+    championshipID  INTEGER REFERENCES CHAMPIONSHIP(championshipID) NOT NULL,
+    points          INTEGER NOT NULL DEFAULT 0,
+    played          INTEGER NOT NULL DEFAULT 0,
+    wins            INTEGER NOT NULL DEFAULT 0,
+    loss            INTEGER NOT NULL DEFAULT 0,
+    tie             INTEGER NOT NULL DEFAULT 0
+);
